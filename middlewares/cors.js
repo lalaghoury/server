4353ce6eea790414432023f9f9146307fac5ffb1
lalaghoury <lala.ghoury@gmail.com>
2024-02-29 19:47:16 -0800

    a

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,13 +1,7 @@
 const cors = require("cors");
 
 const useCors = (app) => {
-  app.use(
-    cors({
-      origin: "https://perfectrecipebyaasil.vercel.app",
-      credentials: true,
-      methods: ["GET", "POST", "PUT", "DELETE"],
-    })
-  );
+  app.use(cors());
 };
 
 module.exports = useCors;
